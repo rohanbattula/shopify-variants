1a9d40cfb5d74efc5ef1f8d98e30f3b82c322767
rohanbattula <rohanbattula@gmail.com>
2021-02-22 23:05:06 -0800

    working var pull

--- src/main/java/rohanaio/Main.java
+++ src/main/java/rohanaio/Main.java
@@ -7,10 +7,11 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		String url =
-		"https://shopnicekicks.com/collections/new-arrivals-1/products/waffle-racer-crater-womens-lifestyle-shoe-black" + ".json";
+		"https://shopnicekicks.com/products/air-jordan-1-high-og-silver-toe-womens-lifestyle-shoe-black-silver" + ".json";
 		
 		ShopifyVariants x = new ShopifyVariants();
-		System.out.println(x.run(url));
+		x.run(url);
+		
 		
 	}
 
--- src/main/java/rohanaio/ShopifyVariants.java
+++ src/main/java/rohanaio/ShopifyVariants.java
@@ -2,8 +2,14 @@ package rohanaio;
 
 import java.io.IOException;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
+
+import org.json.*;
+
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -20,17 +26,30 @@ public class ShopifyVariants {
 	private String url; 
 	
 	private OkHttpClient client = new OkHttpClient();
-	private final Moshi moshi = new Moshi.Builder().build();
-	private final JsonAdapter<Product> jsonAdapter = moshi.adapter(Product.class);
+	//private final Moshi moshi = new Moshi.Builder().build();
+	//private final JsonAdapter<Product> jsonAdapter = moshi.adapter(Product.class);
 
-	String run(String url) throws IOException {
+	String[] run(String url) throws IOException {
 		  Request request = new Request.Builder()
 		      .url(url)
 		      .build();
 
 		  try (Response response = client.newCall(request).execute()) {
 			if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-		    return response.body().string();
+			String jsonString = response.body().string();
+			
+			JsonObject jsonObject = new JsonParser().parse(jsonString).getAsJsonObject();
+			JsonArray vars = jsonObject.getAsJsonObject("product").getAsJsonArray("variants");
+			
+			variants = new String[vars.size()];
+			
+			for(int i =0; i < vars.size(); i++) {
+				System.out.println(vars.get(i).getAsJsonObject().get("id").toString());
+				
+			}
+
+			
+		    return variants;
 		  }
 	}
 	
